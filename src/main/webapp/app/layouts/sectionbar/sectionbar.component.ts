6bb72532d7e76947dd0dd8fd93a7538a6d1a8aae
Daniel Flockhart <57047926+DanielFlockhart@users.noreply.github.com>
2024-03-04 16:43:41 +0000

    Merge branch 'main' of git.cs.bham.ac.uk:team-projects-2023-24/team52
    # Conflicts:
    #	src/main/webapp/app/home/home.component.html

--- src/main/webapp/app/layouts/sectionbar/sectionbar.component.ts
+++ src/main/webapp/app/layouts/sectionbar/sectionbar.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { AccountService } from 'app/core/auth/account.service';
+import { Account } from 'app/core/auth/account.model';
 
 @Component({
   selector: 'app-sectionbar',
   templateUrl: './sectionbar.component.html',
   styleUrls: ['./sectionbar.component.scss'],
 })
-export class SectionbarComponent implements OnInit {
-  constructor() {}
+export class SectionbarComponent implements OnInit, OnDestroy {
+  account: Account | null = null;
+  private readonly destroy$ = new Subject<void>();
 
-  ngOnInit(): void {}
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {
+    this.accountService
+      .getAuthenticationState()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(account => (this.account = account));
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
